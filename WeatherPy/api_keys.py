c08b5ad020c114f126489aa339cc570de462e915
Kelly Shull <kelly.t.shull@gmail.com>
2020-07-28 20:08:36 -0700

    modified:   VacationPy/api_keys.py 	modified:   WeatherPy/WeatherPy.ipynb 	modified:   WeatherPy/api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "26900b7201ef2847e296c736917d30ea"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCQFa65hVQ2EIfbR5g2tCAAWGoIY-VT6G4"
